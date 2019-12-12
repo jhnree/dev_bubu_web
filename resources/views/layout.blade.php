0189e610a6aa6cc7332690a569ca1370b0fad096
jhnree <vjohnree25@gmail.com>
2019-12-12 09:18:37 +0800

    features/home

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -14,50 +14,184 @@
     <title>Document</title>
 </head>
 <body>
+
     <Header class="sticky-top">
         <div>
-        <img class="icon" src="{{asset('img/bubu-we-white.png')}}" alt="" srcset="">
-    </div>
+            <img class="icon" src="{{asset('img/bubu-we-white.png')}}" alt="" srcset="">
+        </div>
     </Header>
-    <div class="content">
-        <div class="row">
-            <div class="col-sm-3 col-12 sidenav shadow px-4">
-                <div class="header">
-                    <div class="row">
-                        <div class="col-3">
-                            <button class="btn text-left">
-                                <i class="fas fa-cog"></i>
-                            </button>
+    <div class="container-fluid px-0">
+        <div id="content">
+            <div class="sidenav shadow p-0">
+                <div class="header-container container-fluid">
+                    <div class="header">
+                        <div class="row">
+                            <div class="col-3">
+                                <button class="btn text-left">
+                                    <i class="fas fa-cog"></i>
+                                </button>
+                            </div>
+                            <div class="col-6">
+                                Chat
+                            </div>
+                            <div class="col-3 text-right">
+                                <button class="btn text-left">
+                                    <i class="fas fa-plus-circle"></i>
+                                </button>
+                            </div>
                         </div>
-                        <div class="col-6">
-                            Chat
-                        </div>
-                        <div class="col-3 text-right">
-                            <button class="btn text-left">
-                                <i class="fas fa-plus-circle"></i>
-                            </button>
+                    </div>
+                    <div class="">
+                        <hr class="m-0">
+                        <div class="search-bar mt-2">
+                            <input class="form-control" type="text" placeholder="Search for people or group">
                         </div>
                     </div>
                 </div>
-                <hr class="m-0">
-                <div class="search-bar mt-2">
-                    <input class="form-control" type="text" placeholder="Search for people or group">
-                </div>
                 <div class="message-list">
-                    <div class="my-3">
-                        <div class="profile d-sm-none d-md-block">
-                                <img src="{{asset('img/user-male.png')}}" alt="" srcset="">
+                    <div class="my-3 myMessage">
+                        <div class="profile">
+                            <img src="{{asset('img/user-male.png')}}" alt="" srcset="">
                         </div>
-                        <div class="message offset-md-3 offset-2">
+                        <div class="message">
                             <div class="name">
                                 <span>Mark Renz Olimberio</span>
+                            </div>
+                            <div class="content">
+                                <span>The standard chunk of Lorem Ipsum </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="my-3 myMessage">
+                        <div class="profile">
+                            <img src="{{asset('img/user-male.png')}}" alt="" srcset="">
+                        </div>
+                        <div class="message">
+                            <div class="name">
+                                <span>Mark Renz Olimberio</span>
+                            </div>
+                            <div class="content">
+                                <span>The standard chunk of Lorem Ipsum </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="my-3 myMessage">
+                        <div class="profile">
+                            <img src="{{asset('img/user-male.png')}}" alt="" srcset="">
+                        </div>
+                        <div class="message">
+                            <div class="name">
+                                <span>Mark Renz Olimberio</span>
+                            </div>
+                            <div class="content">
+                                <span>The standard chunk of Lorem Ipsum </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="my-3 myMessage">
+                        <div class="profile">
+                            <img src="{{asset('img/user-male.png')}}" alt="" srcset="">
+                        </div>
+                        <div class="message">
+                            <div class="name">
+                                <span>Mark Renz Olimberio</span>
+                            </div>
+                            <div class="content">
+                                <span>The standard chunk of Lorem Ipsum </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="my-3 myMessage">
+                        <div class="profile">
+                            <img src="{{asset('img/user-male.png')}}" alt="" srcset="">
+                        </div>
+                        <div class="message">
+                            <div class="name">
+                                <span>Mark Renz Olimberio</span>
+                            </div>
+                            <div class="content">
+                                <span>The standard chunk of Lorem Ipsum </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="my-3 myMessage">
+                        <div class="profile">
+                            <img src="{{asset('img/user-male.png')}}" alt="" srcset="">
+                        </div>
+                        <div class="message">
+                            <div class="name">
+                                <span>Mark Renz Olimberio</span>
+                            </div>
+                            <div class="content">
+                                <span>The standard chunk of Lorem Ipsum </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="my-3 myMessage">
+                        <div class="profile">
+                            <img src="{{asset('img/user-female.png')}}" alt="" srcset="">
+                        </div>
+                        <div class="message">
+                            <div class="name">
+                                <span>Angelica Mae Samson</span>
+                            </div>
+                            <div class="content">
+                                <span>The standard chunk of Lorem Ipsum </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="my-3 myMessage">
+                        <div class="profile">
+                            <img src="{{asset('img/user-female.png')}}" alt="" srcset="">
+                        </div>
+                        <div class="message">
+                            <div class="name">
+                                <span>Angelica Mae Samson</span>
+                            </div>
+                            <div class="content">
+                                <span>The standard chunk of Lorem Ipsum </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="my-3 myMessage">
+                        <div class="profile">
+                            <img src="{{asset('img/user-female.png')}}" alt="" srcset="">
+                        </div>
+                        <div class="message">
+                            <div class="name">
+                                <span>Angelica Mae Samson</span>
+                            </div>
+                            <div class="content">
+                                <span>The standard chunk of Lorem Ipsum </span>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-9 d-none d-sm-block">
-                asd
+            <div class="d-none d-sm-block main">
+                <div class="conversation-head pt-2">
+                    <div class="row">
+                        <div class="col-9">
+                            <div class="name text-center">
+                                <span>Mark Renz Olimberio</span><br>
+                            </div>
+                            <div class="status text-center">
+                                <span>Active Now</span>
+                            </div>
+                        </div>
+                        <div class="col-3 text-center">
+                            <button class="btn">
+                                <img src="{{asset('img/call-update.png')}}" width="17px">
+                            </button>
+                            <button class="btn">
+                                <img src="{{asset('img/videocall.png')}}" width="17px">
+                            </button>
+                            <button class="btn">
+                                <img src="{{asset('img/info.png')}}" width="17px">
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
